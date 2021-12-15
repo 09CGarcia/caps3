b77df93f9f1b8af9c5f2f04d3768abcf5dac0625
Tine <88869201+09CGarcia@users.noreply.github.com>
2021-12-16 00:03:58 +0800

    naging toots

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export default function Banner(){
 			<Row>
 				<Col className="px-0">
 					<Jumbotron fluid className="jumbotron px-3">
-						<h1 className="heading text-center">The Tinker Borb</h1>
+						<h1 className="heading text-center">The Tinker Tutorial</h1>
 						<p className="desc text-center">Makes learning fun and productive.</p>
 						<div className="d-flex justify-content-center">
 							<Button className="btn btn-info" size="lg" as={NavLink} to="/programs">Avail Our Services</Button>
